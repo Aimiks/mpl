a5df4ac81288bda59ad32d79bac3dff234f9eaee
Geoffrey <geoffreydu38@hotmail.fr>
2018-09-22 23:16:28 +0200

    Create proto object for networking

---
+++ mpl/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform spawnPoint;
+	// Use this for initialization
+	void Start () {
+        this.transform.SetPositionAndRotation(spawnPoint.transform.position,Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
